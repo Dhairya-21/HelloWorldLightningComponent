3a57755fdf2a1a05c046801d8c744b36f6baa851
Dhairya <koshtidhairya99@gmail.com>
2023-10-03 12:52:20 +0530

    3-10-23

--- force-app/main/default/aura/AccountWizard/AccountWizardController.js
+++ force-app/main/default/aura/AccountWizard/AccountWizardController.js
@@ -1,24 +1,17 @@
 ({
-    init : function(component,event, helper) {
-        var SS = component.get("v.SelStep");
-        component.set("v.SelStepS", parseInt(SS));
-    }
-    // event.preventDefault();
-    SelectStep1 : function(component, event, helper) {
-        helper.SelectStep(component, 1);
-    },
-    SelectStep2 : function(component, event, helper) {
-        helper.SelectStep(component, 2);
-    },
-    SelectStep3 : function(component, event, helper) {
-        helper.SelectStep(component, 3);
-    },
-    NextStep : function(component, event, helper){
+    // SelectStep1: function (component, event, helper) {
+    //     helper.SelectStep(component, 1);
+    // },
+    // SelectStep2: function (component, event, helper) {
+    //     helper.SelectStep(component, 2);
+    // },
+    // SelectStep3: function (component, event, helper) {
+    //     helper.SelectStep(component, 3);
+    // },
+    NextStep: function (component, event, helper) {
         helper.NextStep(component);
-        var SS = component.get("v.SelStep");
-        component.set("v.SelStepS", parseInt(SS));
     },
-    PrevStep : function(component, event, helper){
+    PrevStep: function (component, event, helper) {
         helper.PrevStep(component);
     }
 })
--- force-app/main/default/aura/AccountWizard/AccountWizardHelper.js
+++ force-app/main/default/aura/AccountWizard/AccountWizardHelper.js
@@ -1,17 +1,19 @@
 ({
-  SelectStep: function (component, num) {
-    console.log("Feature Depriciated ");
-    // component.set("v.SelStep", num);
-    // if (num == 3) {
-    //     component.find("NextBtn").set("v.label", "Finish");
-    // } else {
-    //     component.find("NextBtn").set("v.label", "Next");
-    // }
-    // console.log(num);
-  },
+  // SelectStep: function (component, num) {
+  //   console.log("Feature Depriciated ");
+  //   component.set("v.SelStep", num);
+  //   if (num == 3) {
+  //       component.find("NextBtn").set("v.label", "Finish");
+  //   } else {
+  //       component.find("NextBtn").set("v.label", "Next");
+  //   }
+  //   console.log(num);
+  // },
 
   NextStep: function (component) {
     var step = component.get("v.SelStep");
+
+    // First Step Creating Account
     if (step == 1) {
       component.find("NextBtn").set("v.label", "Next");
       component.find("NextBtn").set("v.variant", "Brand");
@@ -27,10 +29,17 @@
         var state = response.getState();
         if ((state = "SUCCESS")) {
           component.set("v.Acc", response.getReturnValue());
+          component.find("tmsg").showToast({
+            "title": 'Creating Account',
+            "message": 'Your Account has been created'
+          });
         }
       });
       $A.enqueueAction(action);
-    } else if (step == 2) {
+    }
+
+    // Second Step Adding Contact 
+    else if (step == 2) {
       component.find("NextBtn").set("v.label", "Finish");
       component.find("NextBtn").set("v.variant", "Success");
       step += 1;
@@ -46,33 +55,35 @@
         var state = response.getState();
         if ((state = "SUCCESS")) {
           component.set("v.Cont", response.getReturnValue());
+          component.find("tmsg").showToast({
+            "title": 'Contact Created',
+            "message": "Your Contact has been created successfully.",
+          });
         }
       });
       $A.enqueueAction(action);
-    } else if (step == 3) {
+    }
+
+    //Third Step creating event
+    else if (step == 3) {
       var action = component.get("c.CreateEvent");
       action.setParams({
         subject: component.find("Subject").get("v.value"),
-        // start: component.find("start").get("v.value"),
-        start: new Date(),
-        // entd: component.find("endt").get("v.value"),
-        entd: new Date(),
+        start: component.find("start").get("v.value"),
+        endt: component.find("endt").get("v.value"),
         accId: component.get("v.Acc").Id
       });
       action.setCallback(this, function (response) {
         var state = response.getState();
         if ((state == "SUCCESS")) {
-          console.log('event created');
-        }
-        else {
-          console.log('failed');
+          component.find("tmsg").showToast({
+            "title": 'Event Created',
+            "message": "Your Event has been created successfully.",
+          });
         }
       });
       $A.enqueueAction(action);
-      console.log(step);
-      console.log(component.find("Subject").get("v.value"));
-      console.log(component.find("start").get("v.value"));
-      console.log(component.find("endt").get("v.value"));
+
     }
   },
 
