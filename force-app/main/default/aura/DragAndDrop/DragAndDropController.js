7578fcb8249e9d29a499a9e766f93158194e72ff
Dhairya <koshtidhairya99@gmail.com>
2023-10-03 17:48:49 +0530

    3-10-23

--- force-app/main/default/aura/DragAndDrop/DragAndDropController.js
+++ force-app/main/default/aura/DragAndDrop/DragAndDropController.js
@@ -1,18 +1,56 @@
 ({
-//   doInit: function (component, event, helper) {
-//     helper.SetCount(component, "v.Acc")
-//   },
+  //   doInit: function (component, event, helper) {
+  //     helper.SetCount(component, "v.Acc")
+  //   },
+
+  // Search Account Methods
   SearchAcc1: function (component, event, helper) {
     helper.SearchAcc(component, 1);
   },
   SearchAcc2: function (component, event, helper) {
     helper.SearchAcc(component, 2);
   },
-  render1:function(component, event, helper){
-    var i = event.target.value;
-    console.log(i);
+
+  //Finding Contacts
+  render1: function (component, event, helper) {
+    // var id1 = component
+    var i = event.target.id;
+    component.set('v.AccName1', i);
+    helper.SearchCont(component, i, 1);
+    component.set("v.hide1", 2);
   },
-//   render2:function(component, event, helper){
-//     helper.renderIt(component, event, 2);
-//   }
+  render2: function (component, event, helper) {
+    var i = event.target.id;
+    component.set('v.AccName2', i);
+    helper.SearchCont(component, i, 2);
+    component.set("v.hide2", 2);
+  },
+  
+  // Draggable contact methods
+  dStart: function (component, event, helper) {
+    var i = event.target.id;
+    component.set("v.Tcon", i);
+    console.log(component.get("v.Tcon"));
+  },
+
+  ondrop: function (component, event, helper) {
+    event.preventDefault();
+  },
+
+  dOver: function (component, event, helper) {
+    var FAcc = event.target.id;
+    var TCon = component.get("v.Tcon");
+    var action = component.get("c.TransferCon");
+    action.setParams({
+      TConId: TCon,
+      FAccId: FAcc
+    });
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        console.log("Transfer SuccessFull");
+      }
+    });
+    $A.enqueueAction(action);
+  }
 });
--- force-app/main/default/aura/DragAndDrop/DragAndDropHelper.js
+++ force-app/main/default/aura/DragAndDrop/DragAndDropHelper.js
@@ -1,4 +1,5 @@
 ({
+  // Search account using search key and set contact count
   SearchAcc: function (component, num) {
     var searchTerm = component.find("input" + num).get("v.value");
     var action = component.get("c.getAccounts");
@@ -9,30 +10,41 @@
         var list = [];
         var conts = response.getReturnValue();
         for (var k in conts) {
-            list.push({ value: conts[k], key: k });
+          list.push({ value: conts[k], key: k });
         }
-        component.set("v.Acc"+ num , list);
+        component.set("v.Acc" + num, list);
       }
     });
     $A.enqueueAction(action);
   },
 
-  renderIt:function(component, num){
-    var i = event
+  // Search account using account name
+  SearchCont: function (component, i, a) {
+    var action = component.get("c.getContacts");
+    action.setParams({
+      name: i
+    });
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state == "SUCCESS") {
+        component.set("v.Cont" + a, response.getReturnValue());
+      }
+    });
+    $A.enqueueAction(action);
   }
 
-//   SetCount: function (component, Acc) {
-//     var count = component.get("c.getContactsValue");
-//     var AccId = component.find(Acc);
-//     count.setParams({
-//       AccId: AccId
-//     });
-//     count.setCallback(this, function (response) {
-//       var state = response.getState();
-//       if (state === "SUCCESS") {
-//         component.set("v.count", response.getReturnValue());
-//       }
-//     });
-//     $A.enqueueAction(count);
-//   }
+  //   SetCount: function (component, Acc) {
+  //     var count = component.get("c.getContactsValue");
+  //     var AccId = component.find(Acc);
+  //     count.setParams({
+  //       AccId: AccId
+  //     });
+  //     count.setCallback(this, function (response) {
+  //       var state = response.getState();
+  //       if (state === "SUCCESS") {
+  //         component.set("v.count", response.getReturnValue());
+  //       }
+  //     });
+  //     $A.enqueueAction(count);
+  //   }
 });
