136b0e6abf818a8c0b602e9ded47c62d7a5b3920
Dhairya <koshtidhairya99@gmail.com>
2023-10-04 19:02:54 +0530

    4-10-23

--- force-app/main/default/lwc/parentFile/parentFile.js
+++ force-app/main/default/lwc/parentFile/parentFile.js
@@ -1,15 +1,18 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
+import { subscribe, unsubscribe, APPLICATION_SCOPE, MessageContext, publish } from "lightning/messageService";
+// import  { * } from "lightning/messageService";
 
 export default class ParentFile extends LightningElement {
-    // @api passFile();
-    file1;
-    // connectedCallback(){
-    //     this.passFile();
-    // }
-    passFile(){
-        console.log(event.detail.files[0].documentId);
-        this.file1 = event.detail.files[0].documentId;
-        // this.template.querySelector('c-child-file').getFile(event.detail.files[0].documentId);
-        // this.template.querySelector('c-child-File').getFile(20);
-    }
+  // @api passFile();
+  file1;
+  // connectedCallback(){
+  //     this.passFile();
+  // }
+  
+  passFile() {
+    console.log(event.detail.files[0].documentId);
+    this.file1 = event.detail.files[0].documentId;
+    // this.template.querySelector('c-child-file').getFile(event.detail.files[0].documentId);
+    // this.template.querySelector('c-child-File').getFile(20);
+  }
 }
